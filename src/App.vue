9e8c6079fdd79085303da63a2c2e47390704b3a4
noveler17 <noveler17@gmail.com>
2017-07-31 14:38:59 +0900

    글꼴CDN을 HTTPS로 변경, 용량 제한 없이 이미지 번들링

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
 </script>
 
 <style lang="less">
-@import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);
+@import url(https://fonts.googleapis.com/earlyaccess/nanumgothic.css);
 body {
   margin: 0 0 0 0;
 }
